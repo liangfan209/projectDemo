62417ebe1c2ca7c8c08ce92816091fafde9aa3c3
Your Name <liangfan502@sina.com>
2020-10-22 17:27:25 +0800

    init

---
+++ app/src/main/java/com/fan/projectdemo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.fan.projectdemo;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_click)
+    TextView mTvClick;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+
+    @OnClick(R.id.tv_click)
+    public void onViewClicked() {
+        Toast.makeText(this,"hello",Toast.LENGTH_SHORT).show();
+    }
+}
